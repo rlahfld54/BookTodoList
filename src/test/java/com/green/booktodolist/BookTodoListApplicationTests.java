6e2a43e95ded6710d9e3b49d5c318463c5d29c92
황주은 <rlahfld54@naver.com>
2023-06-26 17:53:59 +0900

    first commit

---
+++ src/test/java/com/green/booktodolist/BookTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.booktodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
