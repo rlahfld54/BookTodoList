154560411f205069f2bd5eb093d98bc6ea28aaed
김다율 <kdy12061004@gmail.com>
2023-06-26 17:58:30 +0900

    확인

--- src/main/java/com/green/booktodolist/BookTodoListApplication.java
+++ src/main/java/com/green/booktodolist/BookTodoListApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookTodoListApplication {
     // 바보야
-
+    // 확인
     public static void main(String[] args) {
         SpringApplication.run(BookTodoListApplication.class, args);
     }
