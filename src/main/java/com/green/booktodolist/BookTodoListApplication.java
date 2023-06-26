a04fb2c999fd9580d26fe5acb2240da94282b256
서영기 <matep8@knou.ac.kr>
2023-06-26 17:56:37 +0900

    안뇽

--- src/main/java/com/green/booktodolist/BookTodoListApplication.java
+++ src/main/java/com/green/booktodolist/BookTodoListApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookTodoListApplication {
+    // 바보야
 
     public static void main(String[] args) {
         SpringApplication.run(BookTodoListApplication.class, args);
