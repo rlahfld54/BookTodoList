010e55c0ba421b881b8a1300b7812ff15c5df222
황주은 <rlahfld54@naver.com>
2023-06-29 15:41:31 +0900

    Calendar 데이터 완성 수정 - 황주은

--- src/main/java/com/green/booktodolist/calendar/CalendarService.java
+++ src/main/java/com/green/booktodolist/calendar/CalendarService.java
@@ -7,7 +7,10 @@ import com.green.booktodolist.todoList.model.SelMainVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,20 +26,45 @@ public class CalendarService {
         return todoService.selMain();
     }
 
-    public ArrayList<CalendarDto> getTodoList(){
+    public ArrayList<CalendarDto> getTodoList() {
         // 프론트엔드가 원하는 형식으로 파싱하는 곳
          List<SelCategoryDto> icategoryList = this.GetMain().getIcategory();
 
         ArrayList<CalendarDto> calendarList = new ArrayList<>();
         for (int i = 0; i < icategoryList.size(); i++) {
             CalendarDto dto = new CalendarDto();
+
+            String date = icategoryList.get(i).getStart();
+            String enddate = icategoryList.get(i).getEnd();
+
+            String startdate = null;
+            String endDate = null;
+
+            try {
+                startdate = changeDate(date);
+                endDate = changeDate(enddate);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
             dto.setTitle(icategoryList.get(i).getTitle());
-            dto.setStart(icategoryList.get(i).getStart());
-            dto.setEnd(icategoryList.get(i).getEnd());
+            dto.setStart(startdate);
+            dto.setEnd(endDate);
             dto.setColor(icategoryList.get(i).getColor());
             calendarList.add(i,dto);
         }
 
         return calendarList;
+    }
+
+    public String changeDate(String str) throws ParseException {
+//        String str = "2023-12-25 00:41:19";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = format.parse(str);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");   // yyyy-MM-dd HH:mm:ss
+        String test = formatter.format(date);
+        System.out.println(test);
+        return test;
     }
 }
